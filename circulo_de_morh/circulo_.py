4c16f143a4d6d68e81eaa380657236a8aa75c987
AleUlate <69875354+AleUlate@users.noreply.github.com>
2022-05-20 02:24:21 -0600

    continue with circulo_.py

--- circulo_de_morh/circulo_.py
+++ circulo_de_morh/circulo_.py
@@ -4,11 +4,15 @@ from multiprocessing.spawn import old_main_modules
 import numpy as np  
 import matplotlib.pyplot as plt
 
-o_x = eval(input("Digite el valor del esfuerzo normal en el eje x: "))
-o_y = eval(input("Digite el valor del esfuerzo normal en el eje y: "))
-t_xy = eval(input("Digite el valor del esfuerzo cortante: "))
+#o_x = eval(input("Digite el valor del esfuerzo normal en el eje x: "))
+#o_y = eval(input("Digite el valor del esfuerzo normal en el eje y: "))
+#t_xy = eval(input("Digite el valor del esfuerzo cortante: "))
 
-a = eval(input("Digite el valor del ángulo en grados (digite 0 si no aplica): ")) #angulo cita en grados
+o_x = 5
+o_y = -1
+t_xy = 4
+a = 0
+#a = eval(input("Digite el valor del ángulo en grados (digite 0 si no aplica): ")) #angulo cita en grados
 a_radianes = (a* m.pi)/180
 o_prom = (o_x + o_y)/2 #esfuerzo promedio
 R = t_max = m.sqrt(((o_x - o_y)/2)**2 + t_xy**2) #R and t_max
@@ -31,7 +35,7 @@ def o_maxx (prom, ang, t, ox, oy):
         return o_max_a
 
 #grafica
-def plot(prom, R, ox, oy):
+def plot(prom, R):
     #graficar circulo
     plt.subplot(121)
     ang = np.linspace(0, 2 * np.pi,360) #para que grafique un circulo completo
@@ -44,12 +48,14 @@ def plot(prom, R, ox, oy):
     plt.axis('equal') #ejes a igual escala
     #anotaciones
     plt.annotate("o prom", (prom + 0.1 ,0.1))
-    plt.plot(np.linspace(o_minn(o_prom, a_radianes, t_xy, o_x, o_y), 0), np.linspace(0,0), color = 'c', ls = ':'); plt.annotate("o mín", (o_minn(o_prom, a_radianes, t_xy, o_x, o_y) + R/2 , 0.1), color = 'c')
-    plt.plot(np.linspace(o_maxx(o_prom, a_radianes, t_xy, o_x, o_y), 0), np.linspace(0,0), color = 'g', ls = ':'); plt.annotate("o máx", (o_maxx(o_prom, a_radianes, t_xy, o_x, o_y) - R/2 , 0.1), color = 'g')
-    plt.plot(np.linspace(oy, ox), np.linspace(t_xy,-t_xy), color = 'y', ls = '-'); #plt.annotate("o máx", (o_maxx(o_prom, a_radianes, t_xy, o_x, o_y) - R/2 , 0.1), color = 'y')
+    plt.plot(np.linspace(o_minn(o_prom, a_radianes, t_xy, o_x, o_y), 0), np.linspace(0,0), color = 'r', ls = ':'); plt.annotate("o mín", (o_minn(o_prom, a_radianes, t_xy, o_x, o_y)/2 , 0.1), color = 'r')
+    plt.plot(np.linspace(o_maxx(o_prom, a_radianes, t_xy, o_x, o_y), 0), np.linspace(0,0), color = 'g', ls = ':'); plt.annotate("o máx", (o_maxx(o_prom, a_radianes, t_xy, o_x, o_y)/2 , 0.1), color = 'g')
+    plt.plot(np.linspace(o_y, o_x), np.linspace(t_xy,-t_xy), color = 'y', ls = '-')
+    plt.annotate('alpha', xy=((o_x - o_y)/4, t_xy/2), xycoords='data', xytext=(R/2, 0), textcoords='data', arrowprops=dict(arrowstyle="->", connectionstyle="angle3,angleA=0,angleB=-90"))
+    
     #graficar cuadrado
     plt.subplot(122)
     p2 = plt.plot(x1 + 2, y1, color = 'b')
     plt.axis('off') #sin ejes
     plt.show()
-print(plot(o_prom, R, o_x, o_y))
+print(plot(o_prom, R))
